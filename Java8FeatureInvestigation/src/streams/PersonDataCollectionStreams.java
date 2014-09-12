8c0b40d228d98920938336ba32a7bf80dd890904
Pat Howell <patrick.howell@outlook.com>
2014-09-12 21:28:45 +0100

    Added tests and method for partitioning a collections around an age

--- Java8FeatureInvestigation/src/streams/PersonDataCollectionStreams.java
+++ Java8FeatureInvestigation/src/streams/PersonDataCollectionStreams.java
@@ -1,6 +1,8 @@
 package streams;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import data.Person;
@@ -15,6 +17,11 @@ public class PersonDataCollectionStreams {
 	public String concatenateFirstNamesOfPersonsInCollection( Collection<Person> allPersons ) {
 		return allPersons.stream()
 						 .collect(Collectors.mapping(Person::getFirstName, Collectors.joining(", ")));
+	}
+	
+	public Map<Boolean, List<Person>> sortPersonsIntoSubsetsByGreaterAndLessThanAge( Collection<Person> allPersons, int age ) {
+		return allPersons.stream()
+						 .collect(Collectors.partitioningBy(person -> person.getAge() > age));
 	}
 	
 }
--- Java8FeatureInvestigationTests/src/streams/PersonDataCollectionStreamsTests.java
+++ Java8FeatureInvestigationTests/src/streams/PersonDataCollectionStreamsTests.java
@@ -1,6 +1,8 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -37,13 +39,46 @@ private static PersonDataCollectionStreams personDataCollectionStreams;
 		Assert.assertEquals(expected, result);
 	}
 	
-	//TODO - add method/test for grouping around a boundary. extra test for what happens to entry on the boundary.
+	@Test
+	public void TestPartitioningPersonCollectionAroundAge() {
+		ArrayList<Person> testPeople = createTestPeopleCollectionForAgeTest();
+		
+		Map<Boolean, List<Person>> result = personDataCollectionStreams.sortPersonsIntoSubsetsByGreaterAndLessThanAge( testPeople, 35 );
+		List<Person> underAgeLimit = result.get(false);
+		List<Person> overAgeLimit = result.get(true);
+		Assert.assertEquals(3, underAgeLimit.size());
+		Assert.assertEquals(3, overAgeLimit.size());
+	}
+	
+	@Test
+	public void TestPartitioningPersonsCollectionAroundAgeToMakeEmptyUnderAgeList() {
+		ArrayList<Person> testPeople = createTestPeopleCollectionForAgeTest();
+		
+		Map<Boolean, List<Person>> result = personDataCollectionStreams.sortPersonsIntoSubsetsByGreaterAndLessThanAge( testPeople, 12 );
+		List<Person> underAgeLimit = result.get(false);
+		List<Person> overAgeLimit = result.get(true);
+		Assert.assertEquals(0, underAgeLimit.size());
+		Assert.assertEquals(6, overAgeLimit.size());
+	}
 	
 	private ArrayList<Person> createBasicTestPeopleCollection() {
 		ArrayList<Person> testPeople = new ArrayList<Person>();
 		testPeople.add(new Person("Fred", "Bloggs", 43, new Address(3, "Rutlans Gardens", "Brighton", "BN3 4AP")));
 		testPeople.add(new Person("Urist", "McEntry", 25, new Address(23, "Sun Street", "Whitby", "YO22 4AP")));
 		testPeople.add(new Person("Linda", "someoneElse", 52, new Address(86, "Belgravia Crescent", "Folkstone", "CT17 9QL")));
+		return testPeople;
+	}
+	
+	private ArrayList<Person> createTestPeopleCollectionForAgeTest() {
+		Address dummyAddress = new Address(1, "Somewhere street", "Dartford", "DA1 5PA");
+		
+		ArrayList<Person> testPeople = new ArrayList<Person>();
+		testPeople.add(new Person("Andrew", "Person", 26, dummyAddress));
+		testPeople.add(new Person("Bernard", "Person", 58, dummyAddress));
+		testPeople.add(new Person("Charlie", "Person", 43, dummyAddress));
+		testPeople.add(new Person("Danielle", "Person", 31, dummyAddress));
+		testPeople.add(new Person("Ellie", "Person", 19, dummyAddress));
+		testPeople.add(new Person("Freddy", "Person", 41, dummyAddress));
 		return testPeople;
 	}
 }
