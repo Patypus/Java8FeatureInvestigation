1855010bd0ec70bf642d015b1be3c34a81faccd0
Pat Howell <patrick.howell@outlook.com>
2014-12-07 22:29:10 +0000

    JFI-5: Added comments to ExceptionHandler to explain what it is for.

--- Java8FeatureInvestigation/src/function/ExceptionHandler.java
+++ Java8FeatureInvestigation/src/function/ExceptionHandler.java
@@ -4,6 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+/**
+ * A class using a map of exception types to functions that that can generate messages for the exception type.
+ * This can be used for catch blocks in methods which have to catch multiple exception types but which can only 
+ * throw one type:
+ * public void doSomething() throws ConvinientExceptionType {
+ * 	try {
+ * 		//Something dodgy
+ * 	} catch ( UnhelpfulExceptionType | OtherExceptionType e ) {
+ * 		String usefulMessage = exceptionHandler.getMessageForException( e );
+ *  	throw new ConvinientExceptionType( usefulMessage );
+ * 	}
+ * }
+ * The use of functions allows simple exceptions to be handled with only returning a string message, or complex
+ * operations, such as mining information out of nested causes, to be used through the same interface. The map
+ * with the types against functions allows the required function to easily be found.
+ */
 public class ExceptionHandler {
 
 	private final Map<Class<?>, Function<Exception, String>> exceptionMap;
@@ -20,13 +36,7 @@ public class ExceptionHandler {
 	}
 	
 	/**
-	 * Method to get an appropriate message for a type of exception. Should be used like:
-	 * try {
-	 * 	//Something dodgy
-	 * } catch ( UnhelpfulExceptionType e ) {
-	 * 	String usefulMessage = exceptionHandler.getMessageForException( e );
-	 *  throw new ConvinientExceptionType( usefulMessage );
-	 * }
+	 * Method to get an appropriate message for a type of exception. 
 	 * @param exception - Exception type to handle
 	 * @return relevant string message for exception type
 	 */
