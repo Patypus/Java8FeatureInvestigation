c48eef4d0994bc49801f1c54979fbd323443059c
Pat Howell <patrick.howell@outlook.com>
2014-12-09 22:02:38 +0000

    JFI-6: finished basic tests and implementation of typed value store.

--- Java8FeatureInvestigation/src/functionalinterface/TypedValueStore.java
+++ Java8FeatureInvestigation/src/functionalinterface/TypedValueStore.java
@@ -17,7 +17,29 @@ public class TypedValueStore<T> {
 	}
 	
 	public Value<T> getItemFromDataStore( final String key ) {
-		return null;
+		return store.get( key );
+	}
+	
+	public void addItemToStore( final String key, final Value<T> item ) throws InvalidValueException {
+		if ( ( key != null && !key.equals( "" ) ) && item != null ) {			
+			store.put( key, item );
+		} else {
+			throw createExceptionForInvalidValues( key, item );
+		}
+	}
+	
+	private InvalidValueException createExceptionForInvalidValues( final String key, final Value<T> item ) {
+		String exceptionMessage = "";
+		
+		if ( key == null  ) {
+			exceptionMessage = "Key value cannot be null.";
+		} else if ( key.equals( "" ) ) {
+			exceptionMessage = "Key value cannot be an empty string.";
+		} else if ( item == null ) {
+			exceptionMessage = "Item value cannot be null.";
+		}
+		
+		return new InvalidValueException( exceptionMessage );
 	}
 	
 }
--- Java8FeatureInvestigationTests/src/functionalinterface/TypedValueStoreTests.java
+++ Java8FeatureInvestigationTests/src/functionalinterface/TypedValueStoreTests.java
@@ -22,4 +22,35 @@ public class TypedValueStoreTests {
 		assertEquals( value, result );
 	}
 	
+	@Test
+	public void putValueAddsItemToValueStore() {
+		try {
+			String itemKey = "NewItem";
+			Value<String> stringValue = new Value<>( "Item" );
+			
+			TypedValueStore<String> store = new TypedValueStore<String>();
+			store.addItemToStore( itemKey, stringValue );
+			
+			assertNotNull( store.getItemFromDataStore( itemKey ) );
+			
+		} catch( Exception e ) {
+			fail( "No exceptions expected for test" );
+		}
+	}
+	
+	@Test( expected = InvalidValueException.class )
+	public void putValueWithNullValueThrowsException() throws InvalidValueException {
+		String itemKey = "NewItem";
+		
+		TypedValueStore<String> store = new TypedValueStore<String>();
+		store.addItemToStore( itemKey, null );
+	}
+	
+	@Test( expected = InvalidValueException.class )
+	public void putValueWithEmptyKeyTHrowsException() throws InvalidValueException {
+		Value<Boolean> value = new Value<Boolean>( true );
+		TypedValueStore<Boolean> store = new TypedValueStore<Boolean>();
+		store.addItemToStore( null, value );
+	}
+	
 }
